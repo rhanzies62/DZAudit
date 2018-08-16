9da0765da9a4e21201357c0ed247ba4a7ba26469
franciscebu <tblfranciscebu@gmail.com>
2018-08-16 12:28:19 +0800

    Initial commit

---
+++ DZAudit.Android/obj/Debug/android/src/md55a1ec809766f6ef2416ec4cc2c38f1d9/MainActivity.java
@@ -0,0 +1,47 @@
+package md55a1ec809766f6ef2416ec4cc2c38f1d9;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("DZAudit.Droid.MainActivity, DZAudit.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("DZAudit.Droid.MainActivity, DZAudit.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
